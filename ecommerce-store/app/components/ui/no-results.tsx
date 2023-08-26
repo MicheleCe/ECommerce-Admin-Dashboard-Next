ec77b1520799c9153346dd887080e8f3aa0481eb
MicheleCe <michele.cerqua@hotmail.com>
2023-08-26 13:01:05 +0200

    store che non funziona :(

--- ecommerce-store/app/components/ui/no-results.tsx
+++
@@ -1,9 +0,0 @@
-const NoResults = () => {
-    return(
-        <div className="flex items-center justify-center f-full w-full text-neutral">
-            No Results Found
-        </div>
-    )
-}
-
-export default NoResults;
